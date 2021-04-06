47cdd25cd42e5aba25ae59dbe18ea712f0a46e4a
davidchivadze <datochicho11@gmail.com>
2021-04-06 17:38:44 +0400

    changes Last One

---
+++ Core/Helper/RepositoryHelperClasses/DeviceLogFilter.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.Helper.RepositoryHelperClasses
+{
+    public class DeviceLogFilter
+    {
+        public int? MachineNumber { get; set; }
+        public int? IndRegID { get; set; }
+        public int? dwVerifyMode { get; set; }
+        public int? dwInOutMode { get; set; }
+        public string DateTimeRecord { get; set; }
+    }
+}
